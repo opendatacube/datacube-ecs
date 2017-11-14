6155c13ee4243a4677bcd33053b533e7b1099f5c
Thomas Butler <tombutler.development@gmail.com>
2017-11-14 15:37:24 +1100

    change ip for jupyter to 0.0.0.0

--- datacube-jupyter/run.sh
+++ datacube-jupyter/run.sh
@@ -12,4 +12,4 @@ echo "db_password: $DB_PASSWORD" >> $CONF_FILE
 echo "db_port: $DB_PORT" >> $CONF_FILE
 
 # Start Jupyter
-jupyter notebook --no-browser --ip 127.0.0.1 --allow-root
+jupyter notebook --no-browser --ip 0.0.0.0 --allow-root
