d3a186825c5717b1ef7c81ab6b65993cd0d02836
Thomas Butler <tombutler.development@gmail.com>
2017-11-14 13:43:56 +1100

    add datacube-jupyter and compose script

---
+++ datacube-jupyter/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CONF_FILE="~/.datacube.conf"
+
+# Build Config file
+echo "[datacube]" > $CONF_FILE 
+echo "db_hostname: $DB_HOSTNAME" >> $CONF_FILE 
+echo "db_database: $DB_DATABASE" >> $CONF_FILE 
+echo "db_username: $DB_USERNAME" >> $CONF_FILE 
+echo "db_password: $DB_PASSWORD" >> $CONF_FILE
+echo "db_port: $DB_PORT" >> $CONF_FILE
+
+# Start Jupyter
+jupyter notebook --no-browser --allow-root
