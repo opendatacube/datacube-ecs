ed20b0c51ee926b98fee7288dc7fc4bccb116929
Andrew Hicks <andrew.hicks@ga.gov.au>
2017-11-15 16:55:07 +1100

    Remove spaces from env var declaration

--- datacube-jupyter/configure.sh
+++ datacube-jupyter/configure.sh
@@ -11,7 +11,7 @@ echo "db_username: $DB_USERNAME" >> $CONF_FILE
 echo "db_password: $DB_PASSWORD" >> $CONF_FILE
 echo "db_port: $DB_PORT" >> $CONF_FILE
 
-TARGET_DATA = "LS8_OLI_NBART/10_-44/LS8_OLI_NBART_3577_10_-44_20170816001616000000_v1508468655.nc"
+TARGET_DATA="LS8_OLI_NBART/10_-44/LS8_OLI_NBART_3577_10_-44_20170816001616000000_v1508468655.nc"
 
 aws s3 cp s3://dea-public-data/LS8_OLI_NBART/ls8_nbart_albers.json /opt/data/LS8_OLI_NBART/
 aws s3 cp s3://dea-public-data/LS8_OLI_PQ/ls8_pq_albers.json /opt/data/LS8_OLI_PQ/
