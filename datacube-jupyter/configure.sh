e4a9cfd736b2c218ffb04d0d649001d5998f52ad
Andrew Hicks <andrew.hicks@ga.gov.au>
2017-11-15 16:47:30 +1100

    Recursively copy files from S3

--- datacube-jupyter/configure.sh
+++ datacube-jupyter/configure.sh
@@ -11,14 +11,15 @@ echo "db_username: $DB_USERNAME" >> $CONF_FILE
 echo "db_password: $DB_PASSWORD" >> $CONF_FILE
 echo "db_port: $DB_PORT" >> $CONF_FILE
 
+TARGET_DATA = "LS8_OLI_NBART/10_-44/LS8_OLI_NBART_3577_10_-44_20170816001616000000_v1508468655.nc"
 
 aws s3 cp s3://dea-public-data/LS8_OLI_NBART/ls8_nbart_albers.json /opt/data/LS8_OLI_NBART/
-aws s3 cp s3://dea-public-data/LS8_OLI_PQ/ls8_pq_albers.json /opt/data/LS8_OLI_NBART/
-aws s3 cp s3://dea-public-data//LS8_OLI_NBART/10_-44/LS8_OLI_NBART_3577_10_-44_20170816001616000000_v1508468655.nc /opt/data/LS8_OLI_NBART/10_-44/
+aws s3 cp s3://dea-public-data/LS8_OLI_PQ/ls8_pq_albers.json /opt/data/LS8_OLI_PQ/
+aws s3 cp --recursive "s3://dea-public-data/$TARGET_DATA" "/opt/data/$TARGET_DATA"
 
 datacube system init
 
-datacube product add /opt/data/ls8_nbart_albers.json
+datacube product add /opt/data/LS8_OLI_NBART/ls8_nbart_albers.json
 datacube product add /opt/data/ls8_pq_albers.json
 
-datacube dataset add /opt/data/LS8_OLI_NBART/10_-44/*
+datacube dataset add /opt/data/**/*.nc
