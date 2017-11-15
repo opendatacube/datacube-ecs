0123c70ec0102ad0d31cca40c297f4474c7212d6
Andrew Hicks <andrew.hicks@ga.gov.au>
2017-11-15 16:26:57 +1100

    Add step to sync data

---
+++ datacube-jupyter/configure.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source $HOME/.profile
+
+export CONF_FILE="$HOME/.datacube.conf"
+
+# Build Config file
+echo "[datacube]" > $CONF_FILE 
+echo "db_hostname: $DB_HOSTNAME" >> $CONF_FILE 
+echo "db_database: $DB_DATABASE" >> $CONF_FILE 
+echo "db_username: $DB_USERNAME" >> $CONF_FILE 
+echo "db_password: $DB_PASSWORD" >> $CONF_FILE
+echo "db_port: $DB_PORT" >> $CONF_FILE
+
+
+aws s3 cp s3://dea-public-data/LS8_OLI_NBART/ls8_nbart_albers.json /opt/data/LS8_OLI_NBART/
+aws s3 cp s3://dea-public-data/LS8_OLI_PQ/ls8_pq_albers.json /opt/data/LS8_OLI_NBART/
+aws s3 cp s3://dea-public-data//LS8_OLI_NBART/10_-44/LS8_OLI_NBART_3577_10_-44_20170816001616000000_v1508468655.nc /opt/data/LS8_OLI_NBART/10_-44/
+
+datacube system init
+
+datacube product add /opt/data/ls8_nbart_albers.json
+datacube product add /opt/data/ls8_pq_albers.json
+
+datacube dataset add /opt/data/LS8_OLI_NBART/10_-44/*
--- datacube-jupyter/run.sh
+++ datacube-jupyter/run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 source $HOME/.profile
 
-export CONF_FILE="$HOME/.datacube.conf"
-
-# Build Config file
-echo "[datacube]" > $CONF_FILE 
-echo "db_hostname: $DB_HOSTNAME" >> $CONF_FILE 
-echo "db_database: $DB_DATABASE" >> $CONF_FILE 
-echo "db_username: $DB_USERNAME" >> $CONF_FILE 
-echo "db_password: $DB_PASSWORD" >> $CONF_FILE
-echo "db_port: $DB_PORT" >> $CONF_FILE
-
 # Start Jupyter
 jupyter notebook --no-browser --ip 0.0.0.0 --port 80 --allow-root
