5cedfd432c8274d656f2da913ed738696cb9249f
Robert Armstrong <robert.armstrong@ga.gov.au>
2017-12-12 14:28:00 +1100

    Remove resource definitions from main

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ variable "aws_region" {
 }
 
 module "ecs" {
-  source = "https://github.com/GeoscienceAustralia/terraform-ecs"
+  source = "../terraform-ecs"
 
   # Tags to apply to the resources
   cluster   = "${var.cluster}"
@@ -60,7 +60,6 @@ module "ecs" {
 
   timeout = 6
 
-  # Network Configuration
   vpc_cidr              = "10.0.0.0/16"
   public_subnet_cidrs   = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs  = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
@@ -69,10 +68,10 @@ module "ecs" {
   ssh_ip_address        = "0.0.0.0/0"
 
   # Server Configuration
-  max_size         = 6
+  max_size         = 2
   min_size         = 1
-  desired_capacity = 6
-  instance_type    = "t2.small"
+  desired_capacity = 1
+  instance_type    = "m4.xlarge"
   ecs_aws_ami      = "ami-c1a6bda2"
   enable_jumpbox   = true
 
@@ -87,62 +86,97 @@ module "ecs" {
   db_name = "datacube"
 
   # Service Configuration
-  service_entrypoint = "datacube-wms"
-  service_compose = "docker-compose-aws.yml"
+  service_name = "datacube-wms"
+  # service_entrypoint = "datacube-wms"
+  # service_compose = "docker-compose-aws.yml"
   # custom_script = "export PUBLIC_URL=$${module.load_balancer.alb_dns_name}"
-  health_check_path = "/health"
+  # health_check_path = "/health"
 
-}
-
-resource "aws_ecs_task_definition" "datacube-service-task" {
-  family = "datacube-wms-service-task"
-  container_definitions = <<EOF
-  [
-    {
-    "name": "datacube-wms",
-    "image": "geoscienceaustralia/datacube-wms:latest",
-    "memory": 1536,
-    "essential": true,
-    "portMappings": [
-      {
-        "containerPort": 80
-      }
-    ],
-    "mountPoints": [
-      {
-        "containerPath": "/opt/data",
-        "sourceVolume": "volume-0"
-      }
-    ],
-    "environment": [
-      { "name": "DB_USERNAME", "value": "master" },
-      { "name": "DB_PASSWORD", "value": "${var.db_admin_password}" },
-      { "name": "DB_DATABASE", "value": "datacube" },
-      { "name": "DB_HOSTNAME", "value": "database.local" },
-      { "name": "DB_PORT", "value": "5432" },
-      { "name": "PUBLIC_URL", "value": "${module.ecs.alb_load_balancer_dns}"}
-    ]
-  }
-]
-EOF
-  task_role_arn = "tf_odc_ecs_role"
-  volume {
+  container_image = "geoscienceaustralia/datacube-wms:latest"
+  task_family = "datacube-task-family"
+  task_volume = {
     name = "volume-0",
-    host_path = "/opt/data"
+    host_path = "/opt/data/"
   }
+  task_environment_vars = [
+      { "name"  = "DB_USERNAME"
+        "value" = "master"
+      },
+      { "name"  = "DB_PASSWORD"
+        "value" = "${var.db_admin_password}"
+      },
+      { "name"  = "DB_DATABASE"
+        "value" = "datacube"
+      },
+      { "name"  = "DB_HOSTNAME"
+        "value" = "database.local"
+      },
+      { "name"  = "DB_PORT"
+        "value" = "5432"
+      },
+      { "name"  = "PUBLIC_URL"
+        "value" = "${module.ecs.alb_dns_name}"
+      }
+  ]
+
+  task_mount_points = [
+    {
+      "container_path" = "/opt/data/"
+      "source_volume"  = "volume-0"
+    }
+  ]
+
 }
 
-resource "aws_ecs_service" "datacube-service" {
-  name = "datacube-wms-service"
-  cluster = "${var.cluster}"
-  task_definition = "${aws_ecs_task_definition.datacube-service-task.arn}"
-  desired_count = 6
-  load_balancer {
-    target_group_arn = "${module.ecs.alb_target_group_arn}"
-    container_name = "datacube-wms"
-    container_port = "80"
-  }
-}
+# resource "aws_ecs_task_definition" "datacube-service-task" {
+#   family = "datacube-wms-service-task"
+#   container_definitions = <<EOF
+#   [
+#     {
+#     "name": "datacube-wms",
+#     "image": "geoscienceaustralia/datacube-wms:latest",
+#     "memory": 1536,
+#     "essential": true,
+#     "portMappings": [
+#       {
+#         "containerPort": 80
+#       }
+#     ],
+#     "mountPoints": [
+#       {
+#         "containerPath": "/opt/data",
+#         "sourceVolume": "volume-0"
+#       }
+#     ],
+#     "environment": [
+#       { "name": "DB_USERNAME", "value": "master" },
+#       { "name": "DB_PASSWORD", "value": "${var.db_admin_password}" },
+#       { "name": "DB_DATABASE", "value": "datacube" },
+#       { "name": "DB_HOSTNAME", "value": "database.local" },
+#       { "name": "DB_PORT", "value": "5432" },
+#       { "name": "PUBLIC_URL", "value": "${module.ecs.alb_load_balancer_dns}"}
+#     ]
+#   }
+# ]
+# EOF
+#   task_role_arn = "tf_odc_ecs_role"
+#   volume {
+#     name = "volume-0",
+#     host_path = "/opt/data"
+#   }
+# }
+
+# resource "aws_ecs_service" "datacube-service" {
+#   name = "datacube-wms-service"
+#   cluster = "${var.cluster}"
+#   task_definition = "${aws_ecs_task_definition.datacube-service-task.arn}"
+#   desired_count = 6
+#   load_balancer {
+#     target_group_arn = "${module.ecs.alb_target_group_arn}"
+#     container_name = "datacube-wms"
+#     container_port = "80"
+#   }
+# }
 
 output "dns_name" {
   value = "${module.ecs.alb_dns_name}"
