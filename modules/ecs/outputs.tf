1c4ea54e2ff669765d145ceca4626fef11a4c75a
harshurampur <harshu.rampur@ga.gov.au>
2018-04-23 16:47:54 +1000

    merged ecs_policy and ecs_service to one module -ecs

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -5,3 +5,7 @@ output "alb_security_group_id" {
 output "alb_dns_name" {
   value = "${module.alb.alb_dns_name}"
 }
+
+output "role_arn" {
+  value = "${aws_iam_role.instance_role.arn}"
+}
--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -38,6 +38,11 @@ variable "db_name" {
   description = "database DNS name"
 }
 
+variable "container_name" {
+  type = "string"
+  default = "datacube-wms"
+  description = "Name of the container targetted by the load balancer"
+}
 variable "container_port" {
   default = 80
 }
@@ -97,3 +102,114 @@ variable "owner" {
   type = "string"
   description = "Owner of this cluster"
 }
+
+########
+# ecs_policy
+########
+
+variable "ssm_decrypt_key" {
+  description = "Alias for the ssm decrypt key to access secure ssm parameters"
+  default     = "aws/ssm"
+}
+
+variable "account_id" {
+  description = "The account id for specifying arns"
+}
+
+variable "task_role_name" {
+  description = "The name of the role"
+}
+
+variable "parameter_store_resource" {
+  type = "string"
+  default = "*"
+  description = "The parameter store services that can be acccessed. E.g. * for all or /datacube/* for all datacube"
+}
+
+# Dummy variable to emulate a depends_on
+variable depends_on {
+  default = [],
+  type = "list"
+}
+
+########
+# ecs_service
+########
+
+variable "target_group_arn" {
+  type = "string"
+  description = "ARN of the ELB's target group"
+}
+
+# Task variables
+variable "family" {
+  type = "string"
+  description = "The family of the task"
+}
+
+variable "container_path" {
+  type = "string"
+  default = "/opt/data"
+  description = "file system path to mounted volume on container. e.g. /opt/data"
+}
+
+variable "volume_name" {
+  type = "string"
+  default = "volume-0"
+  description = "name of the volume to be mounted"
+}
+
+variable "task_role_arn" {
+  type = "string"
+  description = "ARN for the role the task will run as"
+}
+
+variable "container_definitions" {
+  type = "string"
+  description = "JSON container definition"
+  # example JSON string
+  #   <<EOF
+  #   [
+  #     {
+  #     "name": "${var.container_name}",
+  #     "image": "${var.container}",
+  #     "memory": ${var.memory},
+  #     "essential": true,
+  #     "portMappings": [
+  #       {
+  #         "containerPort": ${var.container_port}
+  #       }
+  #     ],
+  #     "mountPoints": [
+  #       {
+  #         "containerPath": "${var.container_path}",
+  #         "sourceVolume": "${var.volume_name}"
+  #       }
+  #     ],
+  #     "environment": [
+  #       { "name": "DB_USERNAME", "value": "master" },
+  #       { "name": "DB_PASSWORD", "value": "${var.db_admin_password}" },
+  #       { "name": "DB_DATABASE", "value": "datacube" },
+  #       { "name": "DB_HOSTNAME", "value": "database.local" },
+  #       { "name": "DB_PORT", "value": "5432" },
+  #       { "name": "PUBLIC_URL", "value": "${var.public_url}"}
+  #     ]
+  #   }
+  # ]
+  # EOF
+}
+
+# variable "cpu" {
+#   type = "string"
+#   default = 1024
+# }
+
+# variable "cpu" {
+#   type = "string"
+#   default = 1024
+# }
+
+# variable "launch_type" {
+#   type = "string"
+#   default = "FARGATE"
+# }
