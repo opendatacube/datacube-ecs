610a547c7e6a86c6497cf154033f2c206a6dbda3
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-04-26 13:10:06 +1000

    Add script to archive data in s3 folders which is older than a certain date based on the folder name.

---
+++ datacube-wms/indexing/archive.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Archive datasets in prefixes older than specified number of days
+
+while getopts ":d:b:p" o; do
+    case "${o}" in
+        d)
+            d=${OPTARG}
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        b)
+            b=${OPTARG}
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# d is number of days older than current date
+# calculate date string
+todate=$(date -d"$(date) -${d} day" +%s)
+
+# list of folders with names formated to be %Y-%m-%d
+folders=$(aws s3 ls s3://${b}/${p} | grep "PRE " | awk '{print $2}' | sed 's/\/$//')
+
+# archive data in folders older than todate
+for folder in $folders; do
+    if [ $todate -ge  $(date -d $folder +%s) ]; then
+        python ls_s2_cog.py ${b} --prefix ${p}/$folder --archive
+    fi
+done
+
