ca68f0a68df55ef0178068ef98fbaf75b861ac93
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-04-26 13:14:11 +1000

    mend

--- datacube-wms/indexing/archive.sh
+++ datacube-wms/indexing/archive.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Archive datasets in prefixes older than specified number of days
+# Usage: -d days prior to current date to archive
+#        -p prefix for search
+#        -b bucket containing data
 
 while getopts ":d:b:p" o; do
     case "${o}" in
@@ -21,6 +24,7 @@ shift $((OPTIND-1))
 todate=$(date -d"$(date) -${d} day" +%s)
 
 # list of folders with names formated to be %Y-%m-%d
+# grep for "PRE" to get folders
 folders=$(aws s3 ls s3://${b}/${p} | grep "PRE " | awk '{print $2}' | sed 's/\/$//')
 
 # archive data in folders older than todate
@@ -29,4 +33,3 @@ for folder in $folders; do
         python ls_s2_cog.py ${b} --prefix ${p}/$folder --archive
     fi
 done
-
