2f912b02e13ef6f1441be53061a0565d8e6c5e8f
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-05-02 08:47:44 +1000

    Add config file updating to the indexing script

--- docker/datacube-wms/indexing/assets/update_ranges_wrapper.sh
+++ docker/datacube-wms/indexing/assets/update_ranges_wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Wraps the update_ranges.sh script by passing in environment variables
 # from the Docker's environment variables
-
+./get_wms_config.sh
 indexing/update_ranges.sh -b "$DC_S3_INDEX_BUCKET" -p "$DC_S3_INDEX_PREFIX"
