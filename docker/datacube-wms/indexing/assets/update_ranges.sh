80c9db49f3cc0c227e3c92f8a387f049ebdf910e
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-05-01 13:24:42 +1000

    Update the indexing container to support multiple prefixes.

--- docker/datacube-wms/indexing/assets/update_ranges.sh
+++ docker/datacube-wms/indexing/assets/update_ranges.sh
@@ -2,16 +2,17 @@
 # Index new datasets and update ranges for WMS
 # should be run after archiving old datasets so that
 # ranges for WMS are correct
-# Usage: -p prefix for search
+# environment variables:
+# Usage: -p prefix(es) for search. If multiple use space seperated list enclosed in quotes
 #        -b bucket containing data
-# e.g. ./update_ranges dea-public-data L2/sentinel-2-nrt/S2MSIARD
+# e.g. ./update_ranges -b dea-public-data -p "L2/sentinel-2-nrt/S2MSIARD/2018 L2/sentinel-2-nrt/2017"
 
 usage() { echo "Usage: $0 [-p <prefix>] [-b <bucket>]" 1>&2; exit 1; }
 
 while getopts ":p:b:" o; do
     case "${o}" in
         p)
-            p=${OPTARG}
+            prefixes=${OPTARG}
             ;;
         b)
             b=${OPTARG}
@@ -20,13 +21,16 @@ while getopts ":p:b:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${p}" ] || [ -z "${b}" ]; then
+if [ -z "${prefixes}" ] || [ -z "${b}" ]; then
     usage
 fi
 
 # index new datasets
 # prepare script will add new records to the database
-python /ls_s2_cog.py $b --prefix $p
+for p in $prefixes
+do
+    python indexing/ls_s2_cog.py $b --prefix $p
+done
 
 # update ranges in wms database
-python datacube-wms/update_ranges.py
+python update_ranges.py
