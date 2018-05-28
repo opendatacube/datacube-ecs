915a4d16e6a91f913f65a40661d49a2c8f2c15d5
Thomas Butler <tombutler.development@gmail.com>
2018-05-29 09:38:48 +1000

    Apps share a key and are seperated by workspace

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ pushd infrastructure
 rm -rf .terraform
 export WORKSPACE=$1
 export AWS_PROFILE="$profile"
-terraform init -backend-config workspaces/$WORKSPACE/backend.cfg
+terraform init -backend-config backend-dev.cfg
 terraform workspace new $WORKSPACE || terraform workspace select $WORKSPACE
 terraform apply -auto-approve -input=false -var-file="workspaces/$WORKSPACE/terraform.tfvars"
 unset WORKSPACE
