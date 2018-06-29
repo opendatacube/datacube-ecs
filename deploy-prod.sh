20cc36727baee1f77580c3c1b6e17cee0a5bf60a
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-06-29 14:09:31 +1000

    Abort from deploy/dry-run if workspace directory does not exist

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -7,10 +7,14 @@ pushd infrastructure
 rm -rf .terraform
 export WORKSPACE=$1
 export AWS_PROFILE="$profile"
+if [ ! -d "workspaces/$WORKSPACE" ]
+then
+    echo "Could not find workspace directory $WORKSPACE"
+    echo "Aborting..."
+    exit 1
+fi
 terraform init -backend-config backend-prod.cfg
 terraform workspace new $WORKSPACE || terraform workspace select $WORKSPACE
 terraform plan-input=false -var-file="workspaces/$WORKSPACE/terraform.tfvars" -var 'cluster=datacube-prod'
 terraform apply -auto-approve -input=false -var-file="workspaces/$WORKSPACE/terraform.tfvars" -var 'cluster=datacube-prod'
-unset WORKSPACE
-unset AWS_PROFILE
 popd
